a3505227d5e616dd525780b8354f88fec2694620
Martin Grønlien Pejcoch <martinp@met.no>
2021-03-02 18:45:28 +0100

    fix: Make paths relative to main dir

--- app/run.py
+++ app/run.py
@@ -30,11 +30,11 @@ def tokenize(text):
 
 
 # load data
-engine = create_engine("sqlite:///../data/DisasterResponse.db")
+engine = create_engine("sqlite:///./data/DisasterResponse.db")
 df = pd.read_sql_table("messages", engine)
 
 # load model
-model = joblib.load("../models/classifier.pkl")
+model = joblib.load("./models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
